090d7245c1ae492f339085c04038e738fa8074d2
gazifuad <gazi.fuad@gmail.com>
2023-01-28 21:25:14 -0600

    update cosine_sim

---
+++ cosine_sim.py
@@ -0,0 +1,11 @@
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import CountVectorizer
+
+def cos_sim(pattern_str, text_str):
+    # Initialize vectorizer
+    vectorizer = CountVectorizer().fit_transform([pattern_str, text_str])
+
+    # compute and cosine similarity of texts
+    similarity = cosine_similarity(vectorizer[0:1], vectorizer[1:2])
+    
+    return similarity[0][0]
